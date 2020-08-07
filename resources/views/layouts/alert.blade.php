f22c38fa128d282617a0bb625fa206b07c59993b
Djendra Priono <djendrapriono@hotmail.com>
2020-08-07 19:06:44 +0700

    revisi alert danger menjadi error

--- resources/views/layouts/alert.blade.php
+++ resources/views/layouts/alert.blade.php
@@ -25,7 +25,7 @@
 </div>
 @endif
 
-@if ($message = Session::get('danger'))
+@if ($message = Session::get('error'))
 <div class="alert alert-danger alert-dismissible fade show" role="alert">
     <strong>{{ $message }}</strong>
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
