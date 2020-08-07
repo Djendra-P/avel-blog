8274042b47d328ad780033b822933dc1b8065ea8
Djendra Priono <djendrapriono@hotmail.com>
2020-08-07 19:05:19 +0700

    membuat relasi one to many

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
@@ -10,4 +10,8 @@ class Article extends Model
   protected $fillable = [
     'author_id', 'category_id', 'title', 'slug', 'excerpt', 'body', 'is_publish', 'updated_at', 'created_at'
   ];
+
+  public function user(){
+    return $this->belongsTo(User::class, 'author_id', 'id');
+}
 }
